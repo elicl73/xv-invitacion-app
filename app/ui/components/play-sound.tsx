367c5d2fdd6a54c91a3d1b8507aceeec3e1de9e5
Eli Cruz López <elicl23@msn.com>
2024-06-04 10:52:18 -0700

    Fix Modal

--- app/ui/components/play-sound.tsx
+++ app/ui/components/play-sound.tsx
@@ -5,7 +5,11 @@ import { MdPlayDisabled, MdPlayArrow } from 'react-icons/md'
 
 import { cinzel } from '../fonts'
 
-export default function PlaySound({ closeModal }) {
+interface PlaySoundProps {
+  closeModal: (open: boolean) => void
+}
+
+const PlaySound: React.FC<PlaySoundProps> = ({ closeModal }) => {
   const [play] = useSound('/sounds/SheIsARainbow.mp3')
 
   const handlePlay = () => {
@@ -16,17 +20,23 @@ export default function PlaySound({ closeModal }) {
   return (
     <div className="flex justify-center items-center w-full h-full bg-gray-60 absolute overflow-hidden">
       <div
-        className={`${cinzel.className} flex flex-col items-center gap-14 w-96 h-60 p-6 shadow-xl shadow-black bg-gray-200 rounded-xl`}
+        className={`${cinzel.className} flex flex-col items-center gap-8 w-96 h-60 p-6 shadow-xl shadow-black bg-gray-200 rounded-xl`}
       >
         <span>¿Reproducir Audio?</span>
-        <div className="flex flex-col justify-start items-start h-full">
-          <button onClick={handlePlay}>
+        <div className="flex flex-col justify-start items-start h-full gap-4">
+          <button
+            onClick={handlePlay}
+            className=" bg-indigo-700 h-14 w-52 rounded-md"
+          >
             <div className="flex items-center justify-center gap-2">
               <MdPlayArrow size={18} />
               <span>Si</span>
             </div>
           </button>
-          <button onClick={() => closeModal(false)}>
+          <button
+            onClick={() => closeModal(false)}
+            className="bg-red-700 h-14 w-52 rounded-md"
+          >
             <div className="flex items-center justify-center gap-2">
               <MdPlayDisabled size={18} />
               <span>No</span>
@@ -37,3 +47,5 @@ export default function PlaySound({ closeModal }) {
     </div>
   )
 }
+
+export default PlaySound
